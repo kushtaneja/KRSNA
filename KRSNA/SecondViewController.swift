54f03ff603a5dc9e7ff9f27639d341d87a06bffb
Kush Taneja <kushtaneja16@gmail.com>
2016-11-26 16:17:38 +0530

    Initial Commit

---
+++ KRSNA/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  KRSNA
+//
+//  Created by Kush Taneja on 26/11/16.
+//  Copyright © 2016 Kush Taneja. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
